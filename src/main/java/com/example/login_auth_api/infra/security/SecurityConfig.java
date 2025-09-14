b5e11508b039140f432b6403e311a63cc2b385b9
cauavittor <cauavittorbr@gmail.com>
2025-09-14 00:34:23 -0300

    adicionando o resto dos arquivos

--- src/main/java/com/example/login_auth_api/infra/security/SecurityConfig.java
+++ src/main/java/com/example/login_auth_api/infra/security/SecurityConfig.java
@@ -1,14 +1,21 @@
 package com.example.login_auth_api.infra.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.concurrent.ExecutionException;
 
 @Configuration
 @EnableWebSecurity
@@ -36,12 +43,13 @@ public class SecurityConfig {
 
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
-
-
-
-
-
-
-
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
